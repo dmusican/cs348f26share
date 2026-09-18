a2948fc20ede6bfa865500a87781b00d620fe4b5
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2026-09-18 11:31:06 -0500

    Scripted auto-commit on change (2026-09-18 11:31:06) by gitwatch.sh

--- 2026-09-18/examples/src/main/kotlin/Example1/Example1.kt
+++ 2026-09-18/examples/src/main/kotlin/Example1/Example1.kt
@@ -4,7 +4,7 @@ package Example1 // package for Dave so that multiple copies of function names a
 class ExampleThread(var id: Int): Thread() {
     override fun run() {
         var total = 0L
-        for (j in 0..< 100000000) {
+        for (j in 0..< 10000000000) {
             total += j
         }
         println("Total is $total and I am id $id")
