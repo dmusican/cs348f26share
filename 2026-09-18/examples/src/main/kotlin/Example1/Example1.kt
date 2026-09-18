86104972735c96ce7b5f3273a8aa6b0501403bc8
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2026-09-18 11:56:06 -0500

    Scripted auto-commit on change (2026-09-18 11:56:06) by gitwatch.sh

--- 2026-09-18/examples/src/main/kotlin/Example1/Example1.kt
+++ 2026-09-18/examples/src/main/kotlin/Example1/Example1.kt
@@ -19,7 +19,7 @@ fun main () {
         val myThread = ExampleThread(i)
         //myThread.run() // bad
         myThread.start()
-        myThread.join()
+        myThread.join()  // wait fo
         println("Result form myThread is ${myThread.answer}")
     }
 
