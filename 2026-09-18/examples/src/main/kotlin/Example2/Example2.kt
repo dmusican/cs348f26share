9fa6804674bf46d139deae208bd29fa5a8530e80
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2026-09-18 11:43:40 -0500

    Scripted auto-commit on change (2026-09-18 11:43:40) by gitwatch.sh

--- 2026-09-18/examples/src/main/kotlin/Example2/Example2.kt
+++ 2026-09-18/examples/src/main/kotlin/Example2/Example2.kt
@@ -1,2 +1,11 @@
 package Example2
 
+fun main() {
+    var counter = 0
+    for (i in 0..<20) {
+        val myThread = thread {
+            counter++
+        }
+    }
+    println("Counter is $counter")
+}
