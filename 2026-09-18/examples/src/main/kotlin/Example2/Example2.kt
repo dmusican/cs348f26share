b201bcee4298ec2832db031778281757e9f21066
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2026-09-18 11:49:23 -0500

    Scripted auto-commit on change (2026-09-18 11:49:23) by gitwatch.sh

--- 2026-09-18/examples/src/main/kotlin/Example2/Example2.kt
+++ 2026-09-18/examples/src/main/kotlin/Example2/Example2.kt
@@ -4,6 +4,9 @@ import kotlin.concurrent.thread
 
 // involves a shared variable without protections
 // which is BAD BAD BAD
+// "thread" as we use here isn't the problem directly,
+// but it doesn't give you a lot of tools for doing things
+// right
 fun main() {
     var counter = 0
     for (i in 0..<20) {
