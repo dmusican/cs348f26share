8f657c61a4156818abd49f9270960ccde46a389a
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2026-09-21 12:10:13 -0500

    Scripted auto-commit on change (2026-09-21 12:10:13) by gitwatch.sh

--- shared-examples/src/main/kotlin/day2026_09_21/ForkJoinBasicThreads/ForkJoinBasicThreads.kt
+++ shared-examples/src/main/kotlin/day2026_09_21/ForkJoinBasicThreads/ForkJoinBasicThreads.kt
@@ -1,5 +1,31 @@
 package ForkJoinBasicThreads
 
+class PrimesThread(val arr: List<Int>, val low: Int, val high: Int): Thread() {
+    val SEQUENTIAL_CUTOFF = 1000
+    var answer = 0
+    override fun run() {
+        // Base case: small portion of the list
+        if (high - low < SEQUENTIAL_CUTOFF) {
+            for (i in low..<high) {
+                if (isPrime(i)) {
+                    answer++
+                }
+            }
+        } else {
+            val mid = (low + high)/2
+            val left = PrimesThread(arr, low, mid)
+            var right = PrimesThread(arr, mid, high)
+            left.start()
+            right.start()
+            left.join()
+            right.join()
+            answer = left.answer + right.answer
+        }
+    }
+}
+
+
+
 fun isPrime(n: Int) : Boolean {
     for (i in 2..<n) {
         if (n % i == 0) {
