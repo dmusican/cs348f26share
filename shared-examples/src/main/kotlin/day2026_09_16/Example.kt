d154fb6dbd640815bc436cb8c0bafc960820a1d6
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2026-09-21 10:21:51 -0500

    Scripted auto-commit on change (2026-09-21 10:21:51) by gitwatch.sh

--- shared-examples/src/main/kotlin/day2026_09_16/Example.kt
+++ shared-examples/src/main/kotlin/day2026_09_16/Example.kt
@@ -1,2 +1,44 @@
 package day2026_09_16
 
+class Salary(var employeeName: String?) {
+
+    var annual: Double? = 0.0
+
+    fun giveRaise(percent: Double) {
+        val annualLocal = annual
+        if (annualLocal != null) {
+            annual = annualLocal * percent
+        }
+    }
+    fun getSalaryInEuros(): Double {
+        val annualLocal = annual
+        if (annualLocal != null) {
+            return annualLocal * 0.91
+        } else {
+            return 0.0
+        }
+    }
+
+    fun addXtoEnd() {
+        employeeName = employeeName + "X"
+    }
+}
+
+fun main() {
+    val pay = Salary(null)
+    pay.annual = 100.0
+    println("Salary is " + pay.annual)
+    //println("Euros is " + pay.getSalaryInEuros())
+    println("Salary is " + pay.annual)
+    pay.giveRaise(.10)
+    println("Salary is " + pay.annual)
+    //pay.addXtoEnd()
+    println(pay.employeeName)
+
+//    var thing: Int? = null
+//    if (thing == null) {
+//        thing = 0
+//    } else {
+//        thing = thing + 1
+//    }
+}
